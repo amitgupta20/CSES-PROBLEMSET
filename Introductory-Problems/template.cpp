4d150f576f07b2efa2c857f32214e1af4c5e255e
AMIT GUPTA <56724720+amitgupta20@users.noreply.github.com>
2021-03-26 00:57:31 +0530

    Create template.cpp

---
+++ Introductory-Problems/template.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define floop(i, n) for (int i = 0; i < n; i++)
+void solve(){
+    int n;
+}
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int test;
+    cin >> test;
+    while(test--)
+        solve();
+}
